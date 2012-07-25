f141054efe77c2b21254cb9d31e3b6b40c83f947
TingPing <tngpng@gmail.com>
2012-07-25 18:18:23 -0400

    added alternate definitions

--- XChat/define.py
+++ XChat/define.py
@@ -4,21 +4,20 @@ import xchat
 
 __module_name__ = "Define"
 __module_author__ = "TingPing"
-__module_version__ = "1"
+__module_version__ = "2"
 __module_description__ = "Show word definitions"
 # based on google dictionary script by Sridarshan Shetty - http://sridarshan.co.cc
-# TODO: alternate definitions
 
 def define(word, word_eol, userdata):
 
-	if len(word) == 2:
+	if len(word) >= 2:
 		_word = xchat.strip(word[1])
 		_number = 1
-		# if len(word) >= 3:
-		# 	_number = int(xchat.strip(word[2]))
+		if len(word) >= 3:
+			_number = int(xchat.strip(word[2]))
 	else:
-		xchat.prnt('Define Usage: /define word')
-		#xchat.prnt('	number being alternate definition 1-3')
+		xchat.prnt('Define Usage: /define word [number]')
+		xchat.prnt('	number being alternate definition')
 		return xchat.EAT_ALL
 
 	url="http://www.google.com/dictionary/json?callback=s&q=" + _word + "&sl=en&tl=en&restrict=pr,de&client=te"
@@ -32,19 +31,15 @@ def define(word, word_eol, userdata):
 		webdef=webdef[0]
 		webdef=webdef["entries"]
 		index=1
-		index_list=[1,2,3]
 
 		for i in webdef:
-			if index != _number:
-				break
+			if index == _number:
+				if i["type"]=="meaning":
+					ans=i["terms"]
+					op=ans[0]['text']
+					split=op.split(';')
+					xchat.prnt(_word + ': ' + split[0].strip())
 			index+=1
-		
-			if i["type"]=="meaning":
-				ans=i["terms"]
-				op=ans[0]['text']
-				split=op.split(';')
-				xchat.prnt(_word + ': ' + split[0].strip())
-
 		return xchat.EAT_ALL
 	else:
 		xchat.prnt('Description unavailable for ' + _word)
