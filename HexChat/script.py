737be77e558755473017677314e29e6a870f90b5
TingPing <tingping@tingping.se>
2013-05-02 21:06:22 -0400

    update script.py

--- HexChat/script.py
+++ HexChat/script.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 if sys.version_info[0] < 3:
 	import urllib as request
 else:
@@ -11,7 +12,7 @@ __module_version__ = '3'
 __module_description__ = 'Download scripts'
 
 script_help = 'Script: Valid commands are:\n \
-  			INSTALL script\n \
+			INSTALL script\n \
 			UPDATE script\n \
 			REMOVE script'
 
@@ -24,8 +25,10 @@ addon_sites = ['http://raw.github.com/TingPing/plugins/master/HexChat/',
 				'http://orvp.net/xchat/',
 				'https://raw.github.com/Xuerian/xchat_overwatch/master/']
 
+
 def expand_script(script):
 	return os.path.join(addon_dir, script)
+
 
 def download(script):
 	if script.partition('.')[2] not in addon_types:
@@ -33,10 +36,10 @@ def download(script):
 		return False
 	for site in addon_sites:
 		if request.urlopen(site + script).getcode() == 200:
-			print('Script: Downloading %s...' %script)
+			print('Script: Downloading {}...'.format(script))
 			request.urlretrieve(site + script, expand_script(script))
 			return True
-	print('Script: Could not find %s' %script)
+	print('Script: Could not find {}'.format(script))
 
 
 def script_cb(word, word_eol, userdata):
@@ -49,7 +52,7 @@ def script_cb(word, word_eol, userdata):
 
 	if cmd == 'install':
 		if os.path.exists(expand_script(arg)):
-			print('Script: %s is already installed.' %arg)
+			print('Script: {} is already installed.'.format(arg))
 			return hexchat.EAT_ALL
 		if download(arg):
 			hexchat.command('timer 5 load ' + expand_script(arg))
@@ -67,15 +70,16 @@ def script_cb(word, word_eol, userdata):
 			hexchat.command('unload ' + expand_script(arg))
 			os.remove(expand_script(arg))
 		else:
-			print('Script: %s is not installed.' %arg)
+			print('Script: {} is not installed.'.format(arg))
 	else:
 		hexchat.command('help script')
 
 	return hexchat.EAT_ALL
 
+
 def unload_callback(userdata):
-	print(__module_name__ + ' version ' + __module_version__ + ' unloaded.')
+	print('{} version {} unloaded'.format(__module_name__, __module_version__))
 
 hexchat.hook_command('script', script_cb, help=script_help)
 hexchat.hook_unload(unload_callback)
-hexchat.prnt(__module_name__ + ' version ' + __module_version__ + ' loaded.')
+print('{} version {} loaded'.format(__module_name__, __module_version__))
