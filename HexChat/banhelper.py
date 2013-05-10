486c8c8af6a388f3c59b40f83f92bf00c74fe90b
TingPing <tngpng@gmail.com>
2013-05-09 23:28:00 -0400

    new style formatting

--- HexChat/banhelper.py
+++ HexChat/banhelper.py
@@ -2,7 +2,7 @@ import hexchat
 
 __module_name__ = "Banhelper"
 __module_author__ = "TingPing"
-__module_version__ = "1"
+__module_version__ = "2"
 __module_description__ = "Simplifies banning and quieting"
 
 wasop = False
@@ -22,22 +22,22 @@ def do_op(deop=False):
 	if not deop:
 		if not is_op():
 			wasop = False
-			hexchat.command('cs op %s' %chan)
+			hexchat.command('cs op {}'.format(chan))
 		else:
 			wasop = True
 	else:
 		if not wasop:
-			hexchat.command('timer .5 cs deop %s' %chan)
+			hexchat.command('timer .5 cs deop {}'.format(chan))
 		
 def get_mask(nick):
 	for user in hexchat.get_list('users'):
 		if user.nick == nick:
 			if user.account:
-				return '$a:%s' %user.account
+				return '$a:{}'.format(user.account)
 			elif user.host:
-				return '*!*@*%s*' %user.host
+				return '*!*@*{}*'.format(user.host)
 			else:
-				hexchat.command('who %s %%chtsunfra,152' %nick)
+				hexchat.command('whois {}'.format(nick))
 				print('BH: User info not found, enable irc_who_join or try again.')
 				return None
 
@@ -47,12 +47,12 @@ def ban_cb(word, word_eol, userdata):
 		if mask:
 			do_op()
 			if word[0] == 'ban':
-				hexchat.command('timer .3 mode +b %s' %mask)
+				hexchat.command('timer .3 mode +b {}'.format(mask))
 			elif word[0] == 'kickban': 
-				hexchat.command('timer .3 mode +b %s' %mask)
-				hexchat.command('timer .3 kick %s' %word[1]) 
+				hexchat.command('timer .3 mode +b {}'.format(mask))
+				hexchat.command('timer .3 kick {}'.format(word[1])) 
 			elif word[0] == 'quiet':
-				hexchat.command('timer .3 mode +q %s' %mask)
+				hexchat.command('timer .3 mode +q {}'.format(mask))
 			do_op(deop=True)
 			
 	return hexchat.EAT_HEXCHAT
