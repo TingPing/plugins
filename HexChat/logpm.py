6cb8e6594461e03716515e9bf4170220cd34e6b6
TingPing <tngpng@gmail.com>
2013-05-17 14:08:46 -0300

    Update logpm.py

--- HexChat/logpm.py
+++ HexChat/logpm.py
@@ -9,7 +9,7 @@ def open_cb(word, word_eol, userdata):
 	chan = hexchat.get_info('channel')
 	# Assume nick if not prefixed with #
 	# Use existing pref for nicks I usually ignore (i.e. chanserv)
-	if chan[0] != '#' and chan not in hexchat.get_prefs('irc_no_hilight'):
+	if chan and chan[0] != '#' and chan not in hexchat.get_prefs('irc_no_hilight'):
 		hexchat.command('chanopt text_logging on')
 
 hexchat.hook_print("Open Context", open_cb)
