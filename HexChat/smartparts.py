0c10cf6c356f658481db2ed2b0e5fcf83cc82d3b
TingPing <tngpng@gmail.com>
2013-05-18 17:05:18 -0300

    Update smartparts.py

--- HexChat/smartparts.py
+++ HexChat/smartparts.py
@@ -10,15 +10,13 @@ def join_cb(word, word_eol, userdata):
   return hexchat.EAT_HEXCHAT
 
 def part_cb(word, word_eol, userdata):
-	nick = word[0]
-	lasttalk = 0
 	for user in hexchat.get_list('users'):
-		if user.nick == nick:
-			lasttalk = user.lasttalk
-			break
+		if user.nick == word[0]:
+			if time() - user.lasttalk > 60 * 5:
+				return hexchat.EAT_HEXCHAT
+			else
+				return hexchat.EAT_NONE
 
-	if time() - lasttalk > 60 * 5:
-		return hexchat.EAT_HEXCHAT
 
 hexchat.hook_print('Quit', part_cb)
 hexchat.hook_print('Part', part_cb)
