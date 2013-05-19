dcbc7edbeb992c1052775dd5e5f0731035ec0518
TingPing <tngpng@gmail.com>
2013-05-19 08:11:01 -0300

    Update smartparts.py

--- HexChat/smartparts.py
+++ HexChat/smartparts.py
@@ -6,16 +6,33 @@ __module_author__ = 'TingPing'
 __module_version__ = '0'
 __module_description__ = 'Intelligently hide parts'
 
-def join_cb(word, word_eol, userdata):
-  return hexchat.EAT_HEXCHAT
+def check_notify (nick):
+	for user in hexchat.get_list('notify'):
+		if user.nick == nick:
+			return True
+		
+	return False
 
-def part_cb(word, word_eol, userdata):
+def check_lasttalk (nick):
 	for user in hexchat.get_list('users'):
-		if user.nick == word[0]:
+		if user.nick == nick:
 			if time() - user.lasttalk > 60 * 5:
 				return hexchat.EAT_HEXCHAT
 			else:
 				return hexchat.EAT_NONE
+
+def join_cb(word, word_eol, userdata):
+	if check_notify(word[0]):
+		return hexchat.EAT_NONE
+	else:
+		return hexchat.EAT_HEXCHAT
+	#return check_lasttalk(word[0])
+
+def part_cb(word, word_eol, userdata):
+	if check_notify(word[0]):
+		return hexchat.EAT_NONE
+
+	return check_lasttalk(word[0])
 
 
 hexchat.hook_print('Quit', part_cb)
