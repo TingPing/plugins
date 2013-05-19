abb086c50fd4c9772ceec7ca4368036e05cc3bc1
TingPing <tngpng@gmail.com>
2013-05-19 17:17:29 -0400

    Update smartparts.py

--- HexChat/smartparts.py
+++ HexChat/smartparts.py
@@ -4,7 +4,7 @@ import hexchat
 __module_name__ = 'SmartParts'
 __module_author__ = 'TingPing'
 __module_version__ = '0'
-__module_description__ = 'Intelligently hide parts'
+__module_description__ = 'Intelligently hide parts, joins, and nick changes'
 
 def check_notify (nick):
 	for user in hexchat.get_list('notify'):
@@ -20,6 +20,12 @@ def check_lasttalk (nick):
 				return hexchat.EAT_HEXCHAT
 			else:
 				return hexchat.EAT_NONE
+
+def nick_cb(word, word_eol, userdata):
+	if check_notify(word[0]) or check_notify(word[1]):
+		return hexchat.EAT_NONE
+
+	return check_lasttalk(word[0])
 
 def join_cb(word, word_eol, userdata):
 	if check_notify(word[0]):
@@ -39,3 +45,4 @@ hexchat.hook_print('Quit', part_cb)
 hexchat.hook_print('Part', part_cb)
 hexchat.hook_print('Part with Reason', part_cb)
 hexchat.hook_print('Join', join_cb)
+hexchat.hook_print('Change Nick', nick_cb)
