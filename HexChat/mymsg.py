e697bbbf8c88082815073a5f96af671f2a80aa50
TingPing <TingPing@users.noreply.github.com>
2013-09-12 01:00:44 -0400

    Update mymsg.py

--- HexChat/mymsg.py
+++ HexChat/mymsg.py
@@ -2,10 +2,14 @@ import hexchat
 
 __module_name__ = 'MyMessages'
 __module_author__ = 'TingPing'
-__module_version__ = '2'
-__module_description__ = 'Properly show your messages in znc with privmsg module'
+__module_version__ = '3'
+__module_description__ = 'Properly show your messages in ZNC'
 
 def privmsg_cb(word, word_eol, userdata, attrs):
+	# We only care about private messages, HexChat handles the rest now.
+	if word[2][0] == '#':
+		return
+
 	mynick = hexchat.get_info('nick')
 	sender = word[0].split('!')[0][1:]
 	recipient = word[2]
@@ -13,8 +17,7 @@ def privmsg_cb(word, word_eol, userdata, attrs):
 	msg = word_eol[3][1:]
 
 	if hexchat.nickcmp(sender, mynick) == 0 and hexchat.nickcmp(recipient, mynick) != 0:
-		if recipient[0] != '#':
-			hexchat.command('query -nofocus {}'.format(recipient))
+		hexchat.command('query -nofocus {}'.format(recipient))
 		hexchat.find_context(network, recipient).set()
 
 		if '\001ACTION' in msg:
