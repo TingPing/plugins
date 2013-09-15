4a41cfd4a03fc7ccd76b6faca0d32c30296fa33c
TingPing <TingPing@users.noreply.github.com>
2013-09-15 05:20:59 -0400

    Create wordhl.py

---
+++ HexChat/wordhl.py
@@ -0,0 +1,28 @@
+import hexchat
+
+__module_name__ = 'wordhl'
+__module_author__ = 'TingPing'
+__module_version__ = '1'
+__module_description__ = 'Highlights some words of importance'
+# When you want to notice something, but not really get 'highlighted'
+
+hlwords = ('hexchat', )
+edited = False
+
+def print_cb(word, word_eol, userdata, attr):
+	global edited
+	if edited or attr.time: # Ignore our own events or bouncer playback
+		return
+
+	if any(_word in word[1] for _word in hlwords):
+		for _word in hlwords:
+			msg = word[1].replace(_word, '\00319' + _word + '\00399').strip() # Color green
+
+		edited = True
+		hexchat.emit_print('Channel Message', word[0], msg)
+		edited = False
+
+		hexchat.command('gui color 3')
+		return hexchat.EAT_ALL
+
+hexchat.hook_print_attrs('Channel Message', print_cb)
