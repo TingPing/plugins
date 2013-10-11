a93d269573078a7fb29f2d9f7949c60b514acd70
TingPing <TingPing@users.noreply.github.com>
2013-10-11 01:21:11 -0400

    Update requiresasl.py

--- HexChat/requiresasl.py
+++ HexChat/requiresasl.py
@@ -1,12 +1,12 @@
 import hexchat
 
-__module_name__ = "RequireSASL"
-__module_author__ = "TingPing"
-__module_version__ = "1"
-__module_description__ = "Disconnect if SASL fails"
+__module_name__ = 'RequireSASL'
+__module_author__ = 'TingPing'
+__module_version__ = '1'
+__module_description__ = 'Disconnect if SASL fails'
 
 def response_cb(word, word_eol, userdata):
-	if "fail" in word_eol[0]: # This is flawed but HexChat relies on 904
-		hexchat.command("timer .1 discon") # Delay so disconnect happens after message.
+	if word[1] == '904': # Failed
+		hexchat.command('timer .1 discon') # Delay so disconnect happens after message.
 
-hexchat.hook_print("SASL Response", response_cb)
+hexchat.hook_print('SASL Response', response_cb)
