587d074109b34172586387152a1a1b35a56327b3
TingPing <TingPing@users.noreply.github.com>
2013-12-21 02:58:00 -0500

    Update alias.py

--- HexChat/alias.py
+++ HexChat/alias.py
@@ -1,10 +1,12 @@
+from __future__ import print_function
 import hexchat
 
-__module_name__ = "Alias"
-__module_author__ = "TingPing"
-__module_version__ = "1"
-__module_description__ = "Create aliases for commands"
+__module_name__ = 'Alias'
+__module_author__ = 'TingPing'
+__module_version__ = '2'
+__module_description__ = 'Create aliases for commands'
 
+alias_hooks = {}
 help_msg = 'Alias: Valid commands are:\n \
 					ALIAS name command\n \
 					UNALIAS name\n \
@@ -15,7 +17,8 @@ def load_aliases ():
 	for pref in hexchat.list_pluginpref():
 		if pref[:6] == 'alias_':
 			name = pref[6:]
-			hexchat.hook_command(name, alias_cmd_cb, help='Alias: "%s" is an alias for "%s"' %(name, get_alias(name)))
+			hook = hexchat.hook_command(name, alias_cmd_cb, help='Alias: "{}" is an alias for "{}"'.format(name, get_alias(name)))
+			alias_hooks[name] = hook
 
 def get_alias(name):
 	cmd = hexchat.get_pluginpref('alias_' + name)
@@ -23,28 +26,34 @@ def get_alias(name):
 
 def remove_alias(name, quiet=False):
 	hexchat.del_pluginpref('alias_' + name)
-	if hexchat.unhook(hook):
+	if name in alias_hooks:
+		hook = alias_hooks.get(name)
+		hexchat.unhook(hook)
+		del alias_hooks[name]
 		return True
 	return False
-
 
 def add_alias(name, cmd):
 	hexchat.set_pluginpref('alias_' + name, cmd)
-	if hexchat.hook_command(name, alias_cmd_cb, help='Alias: "%s" is an alias for "%s"' %(name, get_alias(name))):
+	hook = hexchat.hook_command(name, alias_cmd_cb, help='Alias: "{}" is an alias for "{}"'.format(name, get_alias(name)))
+	alias_hooks[name] = hook
+	if hook:
 		return True
 	return False
 
-
 def alias_cmd_cb(word, word_eol, userdata):
-	hexchat.command('%s %s' %(get_alias(word[0]), word_eol[1]))
+	if len(word) > 1:
+		hexchat.command('{} {}'.format(get_alias(word[0]), word_eol[1]))
+	else:
+		hexchat.command(get_alias(word[0]))
 	return hexchat.EAT_HEXCHAT
 
 def unalias_cb(word, word_eol, userdata):
 	if len(word) > 1:
 		if remove_alias(word[1]):
-			print('Alias: %s removed' %word[1])
+			print('Alias: {} removed'.format(word[1]))
 		else:
-			print('Alias: %s not found' %word[1])
+			print('Alias: {} not found'.format(word[1]))
 	else:
 		hexchat.prnt('Alias: Not enough arguements')
 	return hexchat.EAT_ALL
@@ -56,29 +65,28 @@ def alias_cb(word, word_eol, userdata):
 			edited = True
 		if add_alias(word[1], word_eol[2]):
 			if edited:
-				print('Alias: %s edited' %word[1])
+				print('Alias: {} edited'.format(word[1]))
 			else:
-				print('Alias: %s added' %word[1])
+				print('Alias: {} added'.format(word[1]))
 		else:
-			print('Alias: %s failed to hook' %word[1])
+			print('Alias: {} failed to hook'.format(word[1]))
 	else:		
 		print(help_msg)
 	return hexchat.EAT_ALL
 
-# FIXME: Formatting
 def aliases_cb(word, word_eol, userdata):
-	print('\026NAME\t\t\t\tCMD                                                     ')
+	print('\026{0: <10}{1: <50}'.format('NAME', 'CMD'))
 	for pref in hexchat.list_pluginpref():
 		if pref[:6] == 'alias_':
-			print('%s\t\t\t%s' %(pref[6:], get_alias(pref[6:])))
+			print('{0: <10}{1: <50}'.format(pref[6:], get_alias(pref[6:])))
 	return hexchat.EAT_ALL
 		
 def unload_callback(userdata):
-	print(__module_name__ + ' version ' + __module_version__ + ' unloaded.')
+	print(__module_name__, 'version', __module_version__, 'unloaded.')
 
 hexchat.hook_command("alias", alias_cb, help=help_msg)
 hexchat.hook_command("aliases", aliases_cb, help=help_msg)
 hexchat.hook_command("unalias", unalias_cb, help=help_msg)
 hexchat.hook_unload(unload_callback)
 load_aliases()
-print(__module_name__ + ' version ' + __module_version__ + ' loaded.')
+print(__module_name__, 'version', __module_version__, 'loaded.')
