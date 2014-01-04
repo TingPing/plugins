6f3467782f8947c48501b024e44bd49281656b97
TingPing <TingPing@users.noreply.github.com>
2014-01-03 23:00:45 -0500

    Update lastfm.py

--- HexChat/lastfm.py
+++ HexChat/lastfm.py
@@ -24,6 +24,42 @@ lfm_help = """Lastfm Usage:
 USERNAME = hexchat.get_pluginpref('lfm_username')
 KEY = '4847f738e6b34c0dc20b13fe42ea008e'
 
+def print_nowplaying(track, echo=False):
+	try:
+		title = track['name']
+		artist = track['artist']['#text']
+		album = track['album']['#text']
+	except KeyError:
+		print('Lastfm: Song info not found')
+		return
+
+	if echo:
+		cmd = 'echo Lastfm: {} by {} on {}.'.format(title, artist, album)
+	elif hexchat.get_pluginpref('lfm_say'):
+		cmd = 'say Now playing {} by {} on {}.'.format(title, artist, album)
+	else:
+		cmd = 'me is now playing {} by {} on {}.'.format(title, artist, album)
+
+	hexchat.command(cmd)
+
+def get_track():
+	url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecentTracks&user={}&api_key={}&format=json'.format(USERNAME, KEY)
+	try:
+		response = urllib_request.urlopen(url)
+		text = response.read().decode('utf-8')
+		response.close()
+	except urllib_error.HTTPError:
+		return
+
+	data = json.loads(text)
+
+	try:
+		track = data['recenttracks']['track'][0]
+		if track['@attr']['nowplaying']:
+			return track
+	except (IndexError, KeyError):
+		return
+
 def lfm_cb(word, word_eol, userdata):
 	global USERNAME
 	echo = False
@@ -33,48 +69,19 @@ def lfm_cb(word, word_eol, userdata):
 			echo = True
 		else:
 			USERNAME = word[1]
-			hexchat.set_pluginpref('lfm_username', word[1])
-			print('Lastfm: Username set to {}'.format(word[1]))
+			hexchat.set_pluginpref('lfm_username', USERNAME)
+			print('Lastfm: Username set to {}'.format(USERNAME))
 			return hexchat.EAT_ALL
 
 	if not USERNAME:
 		print('Lastfm: No username set, use /lfm <username> to set it')
 		return hexchat.EAT_ALL
-
-	url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecentTracks&user={}&api_key={}&format=json'.format(USERNAME, KEY)
-	try:
-		response = urllib_request.urlopen(url)
-		text = response.read().decode('utf-8')
-		response.close()
-	except urllib_error.HTTPError as err:
-		print('Lastfm Error: {}'.format(err))
-		return hexchat.EAT_ALL
-
-	data = json.loads(text)
-	playing = False
-	try:
-		track = data['recenttracks']['track'][0]
-		if track['@attr']['nowplaying']:
-			playing = True
-	except (IndexError, KeyError):
-		playing = False
-	
-	if not playing:
-		print('Lastfm: No song currently playing')
-		return hexchat.EAT_ALL
 		
-	title = track['name']
-	artist = track['artist']['#text']
-	album = track['album']['#text']
-
-	if echo:
-		cmd = 'echo Lastfm: {} by {} on {}.'.format(title, artist, album)
-	elif hexchat.get_pluginpref('lfm_say'):
-		cmd = 'say Now playing {} by {} on {}.'.format(title, artist, album)
+	track = get_track()
+	if not track:
+		print('Lastfm: No song currently playing')
 	else:
-		cmd = 'me is now playing {} by {} on {}.'.format(title, artist, album)
-	
-	hexchat.command(cmd)
+		print_nowplaying(track, echo)
 		
 	return hexchat.EAT_ALL
 
