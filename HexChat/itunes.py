1213442b56908a16f3ba841a6d5d920c161c36c0
TingPing <TingPing@users.noreply.github.com>
2015-01-25 20:46:15 -0500

    itunes: Improve error handling

--- HexChat/itunes.py
+++ HexChat/itunes.py
@@ -6,15 +6,22 @@ __module_author__ = 'TingPing'
 __module_version__ = '0'
 __module_description__ = 'Announces current song playing in iTunes for Windows'
 
+loaded = False
 try:
 	import win32com.client
+	import pywintypes
+	loaded = True
 	print(__module_name__, 'version', __module_version__, 'loaded.')
 except ImportError:
 	print('You must install pywin32')
 	hexchat.command('timer 0.1 py unload iTunes')
 
 def np_cb(word, word_eol, userdata):
-	itunes = win32com.client.Dispatch('iTunes.Application')
+	try:
+		itunes = win32com.client.Dispatch('iTunes.Application')
+	except pywintypes.com_error:
+		print("itunes: Failed to communicate with itunes. Note that this requires admin access and you should have had a UAC prompt.")
+		return hexchat.EAT_ALL
 
 	# TODO: Settings
 	try:
@@ -26,7 +33,7 @@ def np_cb(word, word_eol, userdata):
 	return hexchat.EAT_ALL
 
 def unload_cb(userdata):
-	print(__module_name__, 'version', __module_version__, 'unloaded.')
+	if loaded: print(__module_name__, 'version', __module_version__, 'unloaded.')
 
 hexchat.hook_command('np', np_cb)
 hexchat.hook_unload(unload_cb)
