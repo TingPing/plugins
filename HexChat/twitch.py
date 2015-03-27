e55c975cd5de309746dedbc2b179ac4cc5e3e8ce
TingPing <TingPing@users.noreply.github.com>
2015-03-27 12:51:55 -0400

    Update twitch.py

--- HexChat/twitch.py
+++ HexChat/twitch.py
@@ -2,7 +2,7 @@ import hexchat
 
 __module_name__ = 'Twitch'
 __module_author__ = 'TingPing'
-__module_version__ = '3'
+__module_version__ = '4'
 __module_description__ = 'Better integration with Twitch.tv'
 # Very much a work in progress...
 
@@ -27,16 +27,26 @@ def twitchOnly(func):
 # Twitch returns a lot of 'unknown command' errors, ignore them.
 @twitchOnly
 def servererr_cb(word, word_eol, userdata):
-	return hexchat.EAT_ALL
+	if word[3] in ('WHO', 'WHOIS'):
+		return hexchat.EAT_ALL
+
+@twitchOnly
+def endofmotd_cb(word, word_eol, userdata):
+	# TODO: This shows the output of commands like /slow but also
+	# tons of other garbage we have to ignore..
+	# hexchat.command('QUOTE TWITCHCLIENT 3')
 
 # Print jtv messages in server tab.
 @twitchOnly
 def privmsg_cb(word, word_eol, userdata):
 	if word[0][1:].split('!')[0] == 'jtv':
-		for chan in hexchat.get_list('channels'):
-			if chan.type == 1 and chan.id == hexchat.get_prefs('id'):
-				chan.context.emit_print('Server Text', word_eol[3][1:])
-		return hexchat.EAT_ALL
+		if word[2][0] != '#':
+			for chan in hexchat.get_list('channels'):
+				if chan.type == 1 and chan.id == hexchat.get_prefs('id'):
+					chan.context.emit_print('Server Text', word_eol[3][1:])
+			return hexchat.EAT_ALL
+		elif word[3] in (':USERCOLOR', ':USERSTATE', ':HISTORYEND', ':EMOTESET', ':CLEARCHAT', ':SPECIALUSER'):
+			return hexchat.EAT_ALL
 
 # Eat any message starting with a '.', twitch eats all of them too.
 @twitchOnly
@@ -62,4 +72,5 @@ for command, alias in aliases.items():
 	hexchat.hook_command(command, command_cb, alias)
 hexchat.hook_print('Your Message', yourmsg_cb)
 hexchat.hook_server('421', servererr_cb)
+#hexchat.hook_server('376', endofmotd_cb)
 hexchat.hook_server('PRIVMSG', privmsg_cb)
