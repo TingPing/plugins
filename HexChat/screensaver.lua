89a2c9334db08f8bce67a48b1eb7a4225bcb166c
TingPing <TingPing@users.noreply.github.com>
2016-03-27 10:47:44 -0400

    Update screensaver.lua

--- HexChat/screensaver.lua
+++ HexChat/screensaver.lua
@@ -6,10 +6,11 @@ hexchat.register("screensaver", "1.0", "Sets user away when the GNOME screensave
 local bus = Gio.bus_get_sync(Gio.BusType.SESSION)
 
 local function on_active_changed(conn, sender, obj_path, interface, signal, param)
-	if (param[1])
+	if param[1] then
 		hexchat.command("allserv away")
 	else
 		hexchat.command("allserv back")
+	end
 end
 
 bus:signal_subscribe(nil, "org.gnome.ScreenSaver", "ActiveChanged", nil, nil,
