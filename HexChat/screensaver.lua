8df3392edcbfc121a2304c9043ddb1f8f1164443
TingPing <TingPing@users.noreply.github.com>
2016-03-27 10:44:12 -0400

    Create screensaver.lua

---
+++ HexChat/screensaver.lua
@@ -0,0 +1,16 @@
+local lgi = require "lgi"
+local Gio = lgi.Gio
+
+hexchat.register("screensaver", "1.0", "Sets user away when the GNOME screensaver is activated")
+
+local bus = Gio.bus_get_sync(Gio.BusType.SESSION)
+
+local function on_active_changed(conn, sender, obj_path, interface, signal, param)
+	if (param[1])
+		hexchat.command("allserv away")
+	else
+		hexchat.command("allserv back")
+end
+
+bus:signal_subscribe(nil, "org.gnome.ScreenSaver", "ActiveChanged", nil, nil,
+                     Gio.DBusSignalFlags.NONE, on_active_changed)
