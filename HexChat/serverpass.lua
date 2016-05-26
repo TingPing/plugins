a4b787dca28f4dfd640ec7019dfc5e2641efe454
Patrick Griffis <tingping@tingping.se>
2016-05-26 18:59:55 -0400

    Rewrite serverpass in lua

---
+++ HexChat/serverpass.lua
@@ -0,0 +1,24 @@
+hexchat.register('ServerPass', '1', 'Alows using server password with another login method')
+
+-- Configure networks here:
+--   The key must match a network name in your network list
+--   Set the value to an empty string to re-use the same network list password
+--   Otherwise set the value to any string for a custom password
+local NETWORKS = {
+	['freenode'] = '',
+--	['freenode'] = 'password',
+}
+
+hexchat.hook_print('Connected', function (args)
+	local network = hexchat.get_info('network')
+	local username = hexchat.prefs['irc_user_name']
+	local password = NETWORKS[network]
+
+	if password then
+		if password == '' then
+			password = hexchat.get_info('password')
+		end
+
+		hexchat.command(string.format('QUOTE PASS %s:%s', username, password))
+	end
+end)
