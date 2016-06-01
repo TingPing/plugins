1552de35214e42c05fb48e74e2a7081589510e3c
Patrick Griffis <tingping@tingping.se>
2016-05-31 23:02:36 -0400

    nowplaying.lua: Make fully async

--- HexChat/nowplaying.lua
+++ HexChat/nowplaying.lua
@@ -2,94 +2,136 @@ local lgi = require('lgi')
 local GLib = lgi.require('GLib')
 local Gio = lgi.require('Gio')
 
-hexchat.register('NowPlaying', '1', 'Announce songs from MPRIS2 clients')
+hexchat.register('NowPlaying', '2', 'Announce songs from MPRIS2 clients')
 
--- TODO: Everything Async
-local bus = Gio.bus_get_sync(Gio.BusType.SESSION)
+local bus
+local cancellable = Gio.Cancellable()
 
-local function get_players ()
-	local ret, err = bus:call_sync('org.freedesktop.DBus', -- bus
-	                               '/org/freedesktop/DBus', -- object
-	                               'org.freedesktop.DBus', -- interface
-	                               'ListNames', -- method
-	                               nil, -- params
-	                               GLib.VariantType('(as)'), -- return type
-	                               Gio.DBusCallFlags.NONE, 1000)
+Gio.bus_get(Gio.BusType.SESSION, cancellable, function (object, result)
+	local connection, err = Gio.bus_get_finish(result)
 
 	if err then
-		print('NP: Error ' .. tostring(err))
-		return nil
-	elseif #ret ~= 1 then
-		return nil
-	end
-
-	local players = {}
-	local array = ret.value[1]
-	for i = 1, #array do
-		local player_name = array[i]:match('^org%.mpris%.MediaPlayer2%.([^.]+)$')
-		if player_name then
-			players[#players + 1] = player_name
-		end
-	end
-
-	if #players == 0 then
-		return nil
+		local front_ctx = hexchat.find_context(nil, nil)
+		front_ctx:print('NP: Error connecting to dbus: ' .. tostring(err))
 	else
-		return players
+		bus = connection
 	end
+end)
+
+local function get_players (callback)
+	bus:call('org.freedesktop.DBus', -- bus
+		'/org/freedesktop/DBus', -- object
+		'org.freedesktop.DBus', -- interface
+		'ListNames', -- method
+		nil, -- params
+		GLib.VariantType('(as)'), -- return type
+		Gio.DBusCallFlags.NONE, -1,
+		cancellable,
+		function (connection, result)
+			local ret, err = connection:call_finish(result)
+
+			if err then
+				-- print('NP: Error ' .. tostring(err))
+				return
+			elseif #ret ~= 1 then
+				return
+			end
+
+			local players = {}
+			local array = ret.value[1]
+			for i = 1, #array do
+				local player_name = array[i]:match('^org%.mpris%.MediaPlayer2%.([^.]+)$')
+				if player_name then
+					players[#players + 1] = player_name
+				end
+			end
+
+			if #players == 0 then
+				callback(nil)
+			else
+				callback(players)
+			end
+		end)
 end
 
 local function print_nowplaying (player)
-	local ret, err = bus:call_sync('org.mpris.MediaPlayer2.' .. player,
-	                               '/org/mpris/MediaPlayer2',
-	                               'org.freedesktop.DBus.Properties',
-	                               'Get',
-	                               GLib.Variant('(ss)', {'org.mpris.MediaPlayer2.Player', 'Metadata'}),
-	                               GLib.VariantType('(v)'), 
-	                               Gio.DBusCallFlags.NONE, 1000)
+	local original_context = hexchat.props.context
 
-	if err then
-		print('NP: Error ' .. tostring(err))
-		return
-	elseif #ret ~= 1 then
-		return
-	end
+	bus:call('org.mpris.MediaPlayer2.' .. player,
+		'/org/mpris/MediaPlayer2',
+		'org.freedesktop.DBus.Properties',
+		'Get',
+		GLib.Variant('(ss)', {'org.mpris.MediaPlayer2.Player', 'Metadata'}),
+		GLib.VariantType('(v)'),
+		Gio.DBusCallFlags.NONE, -1,
+		cancellable,
+		function (connection, result)
+			local ret, err = connection:call_finish(result)
 
-	local metadata = ret[1].value -- a{sv}
-	local title = metadata['xesam:title'] or 'Unknown Title'
-	local album = metadata['xesam:album'] or 'Unknown Album'
-	local artist
-	if metadata['xesam:artist'] then
-		artist = metadata['xesam:artist'][1]
-	else
-		artist = 'Unknown Artist'
-	end
+			if err then
+				-- print('NP: Error ' .. tostring(err))
+				return
+			elseif #ret ~= 1 then
+				return
+			end
 
-	-- TODO: Support customizing the command
-	hexchat.command(string.format('me is now playing %s by %s on %s.', title, artist, album))
+			local metadata = ret[1].value -- a{sv}
+			local title = metadata['xesam:title'] or 'Unknown Title'
+			local album = metadata['xesam:album'] or 'Unknown Album'
+			local artist
+			if metadata['xesam:artist'] then
+				artist = metadata['xesam:artist'][1]
+			else
+				artist = 'Unknown Artist'
+			end
+
+			if not original_context:set() then
+				return
+			end
+
+			-- TODO: Support customizing the command
+			hexchat.command(string.format('me is now playing %s by %s on %s.', title, artist, album))
+		end)
 end
 
 hexchat.hook_command('np', function (word, word_eol)
-	local players = get_players()
+	if not bus then
+		print('NP: Connection to dbus not yet established')
+		return hexchat.EAT_ALL
+	end
 
-	if not players then
-		print('NP: No player found running.')
-	elseif #word > 1 then
-		local player = word[2]:lower()
-
-		for _, name in pairs(players) do
-			if player == name:lower() then
-				print_nowplaying(name)
-				return hexchat.EAT_ALL
-			end
+	local original_context = hexchat.props.context
+	get_players (function (players)
+		if not original_context:set() then
+			return -- If the tab was closed just don't care
 		end
 
-		print('NP: Player ' .. word[2] .. ' not found.')
-	elseif #players == 1 then
-		print_nowplaying(players[1])
-	else
-		print('NP: You have multiple players running, please specify a name:\n\t' .. tostring(players))
-	end
+		if not players then
+			print('NP: No player found running.')
+		elseif #word > 1 then
+			local player = word[2]:lower()
+
+			for _, name in pairs(players) do
+				if player == name:lower() then
+					print_nowplaying(name)
+					return
+				end
+			end
+
+			print('NP: Player ' .. word[2] .. ' not found.')
+		elseif #players == 1 then
+			print_nowplaying(players[1])
+		else
+			print('NP: You have multiple players running, please specify a name:\n\t' .. tostring(players))
+		end
+	end)
 
 	return hexchat.EAT_ALL
 end, 'NP [player]')
+
+hexchat.hook_unload (function ()
+	-- FIXME: Seems this can possibly crash
+	-- /np
+	-- /lua unload ~/.config/hexchat/addons/nowplaying.lua
+	cancellable:cancel()
+end)
