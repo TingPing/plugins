6bf9ccdb4d52f0cef5e64cc3cf6b5bb902accadb
Patrick Griffis <tingping@tingping.se>
2016-07-22 21:46:07 -0400

    mymsg.lua: Misc improvements

--- HexChat/mymsg.lua
+++ HexChat/mymsg.lua
@@ -1,29 +1,43 @@
-hexchat.register('MyMessage', '1', 'Properly show your own messages in ZNC playback')
+hexchat.register('MyMessage', '2', 'Properly show your own messages in ZNC playback')
 
 hexchat.hook_print('Capability List', function (args)
 	if args[2]:find('znc.in/self%-message') then
 		hexchat.command('CAP REQ znc.in/self-message')
+
+		hexchat.hook_timer(1, function ()
+			-- Emit right after this event
+			hexchat.emit_print('Capability Request', 'znc.in/self-message')
+		end)
 	end
 end)
 
+local function prefix_is_channel (prefix)
+	local chantypes = hexchat.props['chantypes']
+	for i = 1, #chantypes do
+		if chantypes[i] == prefix then
+			return true
+		end
+	end
+	return false
+end
+
 hexchat.hook_server_attrs('PRIVMSG', function (word, word_eol, attrs)
 	-- Only want private messages
-	if word[3]:sub(1, 1) == '#' then
-		return -- TODO: More robust check
+	if prefix_is_channel(word[3]:sub(1, 1)) then
+		return
 	end
 
 	local mynick = hexchat.get_info('nick')
 	local sender = word[1]:match('^:([^!]+)')
 	local recipient = word[3]
-	local network = hexchat.get_info('network')
-	local message = word_eol[4]
-	if message:sub(1, 1) == ':' then
-		message = message:sub(2)
-	end
 
 	if hexchat.nickcmp(sender, mynick) == 0 and hexchat.nickcmp(recipient, mynick) ~= 0 then
 		hexchat.command('query -nofocus ' .. recipient)
-		local ctx = hexchat.find_context(network, recipient)
+		local ctx = hexchat.find_context(hexchat.get_info('network'), recipient)
+		local message = word_eol[4]
+		if message:sub(1, 1) == ':' then
+			message = message:sub(2)
+		end
 
 		if message:sub(1, 8) == '\001ACTION ' then
 			local action = message:sub(9, #message-1)
