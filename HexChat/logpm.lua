10907b4c7deabe8a2d7545e4fccf39fd935acfaa
Patrick Griffis <tingping@tingping.se>
2016-08-01 16:38:04 -0400

    logpm: Improve ignoring non-queries

--- HexChat/logpm.lua
+++ HexChat/logpm.lua
@@ -1,11 +1,13 @@
-hexchat.register('LogPM', '1', 'Automatically log private queries')
+hexchat.register('LogPM', '2', 'Automatically log private queries')
 
 hexchat.hook_print('Open Context', function (args)
-	local channel = hexchat.get_info('channel')
-
-	-- Ignore channels, empty tabs, znc queries, and scripting consoles
-	if not channel:match('^[#%*%(>]') then
-		hexchat.command('chanopt -quiet text_logging on')
+	-- We only want queries
+	if hexchat.props['type'] ~= 3 then
+		return
 	end
 
+	-- Ignore empty tabs, znc queries, and scripting consoles
+	if not hexchat.get_info('channel'):match('^[%*%(>]') then
+		hexchat.command('chanopt -quiet text_logging on')
+	end
 end)
