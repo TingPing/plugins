ca8da6ccc702a8bd31580455a37f796e47f1c222
Patrick Griffis <tingping@tingping.se>
2016-10-24 01:56:59 -0400

    masshighlightignore.lua: Simplify for 2.12.2

--- HexChat/masshighlightignore.lua
+++ HexChat/masshighlightignore.lua
@@ -1,27 +1,34 @@
-hexchat.register('MassHighlightIgnore', '1', 'Ignore mass highlight spam')
+hexchat.register('MassHighlightIgnore', '2', 'Ignore mass highlight spam')
 
 local MAX_COUNT = 4
 
-local function match_nick_in_message (nick, message)
-	local patterns = {
-		'%s' .. nick .. '%s',
-		'^'  .. nick .. '%s',
-		'%s' .. nick .. '$',
-		'^'  .. nick .. '%s',
-	} -- match nick at start, end, or in the message
-	for _, pattern in pairs(patterns) do
-		if message:find(pattern) then
+-- http://lua-users.org/wiki/SplitJoin
+local function split (str)
+	local t = {}
+	local function helper(word)
+		table.insert(t, word)
+		return ''
+	end
+	if not str:gsub('%w+', helper):find('%S') then
+		return t
+	end
+end
+
+local function nick_in_list (nick, list)
+	for word, _ in pairs(list) do
+		if hexchat.nickcmp(word, nick) == 0 then
 			return true
 		end
 	end
 	return false
 end
 
-local function is_mass_highlight (context, message)
+local function is_mass_highlight (message)
 	local count = 0
+	local words = split(message)
 
-	for user in context:iterate('users') do
-		if match_nick_in_message (user.nick, message) then
+	for user in hexchat.props.context:iterate('users') do
+		if nick_in_list(user.nick, words) then
 			count = count + 1
 			if count == MAX_COUNT then
 				return true
@@ -32,40 +39,17 @@ local function is_mass_highlight (context, message)
 	return false
 end
 
-local function is_highlight (context, message)
-	local nick = context:get_info('nick')
-
-	if match_nick_in_message(nick, message) then
-		return true
+local function ignore_mass_hilight (args, attrs, event)
+	if is_mass_highlight(args[2]) then
+		hexchat.emit_print_attrs(attrs, event, unpack(args))
+		return hexchat.EAT_ALL
 	end
-
-	return false
 end
 
-local function ignore_highlight (nick)
-	local ignore_list = hexchat.prefs['irc_no_hilight']
-	local new_list
+hexchat.hook_print_attrs('Channel Msg Hilight', function (args, attrs)
+	return ignore_mass_hilight(args, attrs, 'Channel Message')
+end, hexchat.PRI_HIGHEST)
 
-	if ignore_list == '' then
-		new_list = nick
-	else
-		new_list = ignore_list .. ',' .. nick
-	end		
-
-	hexchat.command('set -quiet irc_no_hilight ' .. new_list)
-	hexchat.command('timer .1 set -quiet irc_no_hilight ' .. ignore_list)
-end
-
-hexchat.hook_server('PRIVMSG', function (word, word_eol)
-	local channel = word[3]
-	local ctx = hexchat.find_context(hexchat.get_info('network'), channel)
-
-	if not ctx then
-		return
-	end
-
-	local message = hexchat.strip(word_eol[4])
-	if is_highlight (ctx, message) and is_mass_highlight (ctx, message) then
-		ignore_highlight (word[1]:match('^:([^!]+).*$'))
-	end
-end)
+hexchat.hook_print('Channel Action Hilight', function (args, attrs)
+	return ignore_mass_hilight(args, attrs, 'Channel Action')
+end, hexchat.PRI_HIGHEST)
