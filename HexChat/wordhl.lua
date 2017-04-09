3340017b2e9215ba8d4a449e54ad164cbf59a6ba
TingPing <tingping@tingping.se>
2017-04-09 12:36:59 -0400

    wordhl: Implement changes from @learn-more

--- HexChat/wordhl.lua
+++ HexChat/wordhl.lua
@@ -1,53 +1,48 @@
 hexchat.register('WordHL', '1', 'Highlights some words of importance')
 -- When you want to notice something, but not really get 'highlighted'
 
+local function nocase (s)
+      s = string.gsub(s, "%a", function (c)
+            return string.format("[%s%s]", string.lower(c),
+                                           string.upper(c))
+          end)
+      return '(' .. s .. ')'
+end
+
 local HLWORDS = {
-	'hexchat',
-	'HexChat',
-} -- case sensitive
+    nocase('hexchat'),
+} -- case insensitive
 local COLOR = '20' -- Red
 
-
-local function replace_word (message, word, replacement)
-	local patterns = {
-		['^'  .. word .. '%s'] = replacement .. ' ',
-		['^'  .. word .. '$'] = replacement,
-		['%s' .. word .. '%s'] = ' ' .. replacement .. ' ',
-		['%s' .. word ..  '$'] = ' ' .. replacement,
-	} -- Match word at start, middle, or end of string
-
-	for pattern, sub in pairs(patterns) do
-		message = message:gsub(pattern, sub)
-	end
-
-	return message
+local function replace_x(s)
+    return '\003' .. COLOR .. s .. '\00399'
 end
 
 local event_edited = false
 local function on_message (args, event_type)
-	if event_edited then
-		return -- Ignore own events
-	end
+    if event_edited then
+        return -- Ignore own events
+    end
 
-	local message = args[2]
-	for _, word in ipairs(HLWORDS) do
-		message = replace_word (message, word, '\003' .. COLOR .. word .. '\00399')
-	end
+    local message = args[2]
+    for _, word in ipairs(HLWORDS) do
+        message = message:gsub("%f[%w_]" .. word .. "%f[^%w_]", replace_x)
+    end
 
-	if message ~= args[2] then
-		event_edited = true
-		args[2] = message
-		hexchat.emit_print(event_type, unpack(args))
-		event_edited = false
+    if message ~= args[2] then
+        event_edited = true
+        args[2] = message
+        hexchat.emit_print(event_type, unpack(args))
+        event_edited = false
 
-		hexchat.command('gui color 3')
-		return hexchat.EAT_ALL
-	end
+        hexchat.command('gui color 3')
+        return hexchat.EAT_ALL
+    end
 
 end
 
 for _, event in ipairs({'Channel Action', 'Channel Message'}) do
-	hexchat.hook_print(event, function (args)
-		return on_message (args, event)
-	end, hexchat.PRI_HIGH)
+    hexchat.hook_print(event, function (args)
+        return on_message (args, event)
+    end, hexchat.PRI_HIGH)
 end
